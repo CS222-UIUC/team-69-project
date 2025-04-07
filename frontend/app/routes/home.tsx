37eea5980ca5295038c4cc5785b087fb30c88b90
Cameron <camasher22@gmail.com>
2025-04-06 20:48:09 -0500

    formatting

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,6 +1,6 @@
 import type { Route } from './+types/home';
 import { Welcome } from '../welcome/welcome';
-import {Signup} from "../sign_up/sign_up";
+import { Signup } from '../sign_up/sign_up';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,4 +12,3 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return <Signup />;
 }
-
